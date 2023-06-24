5125e9f92c8319774393eddb5e80a8ee52eb9912
TurBoss <j.l.toledano.l@gmail.com>
2023-06-24 20:59:24 +0200

    clean Update badapple.bin and main.c

--- src/main.c
+++ src/main.c
@@ -2,64 +2,60 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
-//#include <string.h>
-//#include <time.h>
-//#include <fcntl.h>
-//#include <unistd.h>
 
 #include <graphics.h>
 
+#define LINE_CHARS 46
+#define LINES_FRAME 30
 
-#define OFFSET_X 25
-#define OFFSET_Y 25
-
-#define SPACE_X 8
-#define SPACE_Y 8
-
-
-#define EQUALS(x, y) (strcmp(x, y) == 0)
+#define OFFSET_X 30
+#define OFFSET_Y 10
 
 
 int main(int argc, char const *argv[])
 {
 
-//	int screen_w = getmaxx();
-//	int screen_h = getmaxy();
-//
+	int screen_w = getmaxx();
+	int screen_h = getmaxy();
+
+	int pixel_w = screen_w / LINE_CHARS ;
+	int pixel_h = screen_h / LINES_FRAME;
+
 //	printf("%d %d", screen_w, screen_h);
 
     int x = 0;
     int y = 0;
 
 	char *file_path = "badapple.txt";
-	char line[46];
+
+	char line[LINE_CHARS];
     char c = "";
 
 	FILE *fp= fopen(file_path, "r");
 
 	if ( fp == NULL ) {
-        printf("Can't open file %s\n",file_path);
+        printf("Can't open file %s\n", file_path);
         return 1;
     }
 
-    while(fgets(line, sizeof(line), fp) != NULL) { /* read a line from a file */
+    while(fgets(line, sizeof(line), fp) != NULL) {
 
     	if (line[0] == '*') {
-        	// clg();
-    		// msleep();
+    		//msleep(10000);
+    		//clg();
     		y = 0;
     	}
     	else {
-			for (x = 0; x < 45; x++) {
-				c = line[x];
+    		for(x = 0; x < LINE_CHARS; x++) {
+    			c = line[x];
 
-				if ((c == '1')) {
-					unplot((x * SPACE_X) + OFFSET_X, (y * SPACE_Y) + OFFSET_Y);
+				if (c == '1') {
+					unplot((x * pixel_w) + OFFSET_X, (y * pixel_h) +OFFSET_Y);
 				}
-				else if ((c == '0')) {
-					plot((x * SPACE_X) + OFFSET_X, (y * SPACE_Y) + OFFSET_Y);
+				else if (c == '0') {
+					plot((x * pixel_w) + OFFSET_X, (y * pixel_h) +OFFSET_Y);
 				}
-			}
+    		}
 			y++;
     	}
     }
